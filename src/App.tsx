a50ebf84ca352b804ab94d23399f73b5ba835ce5
Murthy <murthy@vectorsoft.com>
2025-10-29 13:36:52 -0500

    import fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Navbar from './components/ui/Navbar';
 import Footer from './components/ui/Footer';
